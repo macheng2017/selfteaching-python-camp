fe71c09ea8c08e2040c3d6834ed45fef7eea19b8
cheng ma <macheng2025@gmail.com>
2019-04-08 17:50:18 +0800

    +++

--- 19100302/macheng2017/d13_training3.py
+++ 19100302/macheng2017/d13_training3.py
@@ -39,13 +39,13 @@ def print_others(msg):
     #     pass
     print(msg)
 
-# # # 自动接受新的好友请求
+# # 自动接受新的好友请求
 
-# @bot.register(msg_types=FRIENDS)
-# def auto_accept_friends(msg):
-#     # 接受好友请求
-#     new_friend = msg.card.accept()
-#     # 向新的好友发送消息
-#     new_friend.send('哈哈，我自动接受了你的好友请求')
+@bot.register(msg_types=FRIENDS)
+def auto_accept_friends(msg):
+    # 接受好友请求
+    new_friend = msg.card.accept()
+    # 向新的好友发送消息
+    new_friend.send('哈哈，我自动接受了你的好友请求')
 
 embed()
